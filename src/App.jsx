a0d83a3b99e41a23c9edfc794cc4ed867bf6d573
milan-2604 <milan2003oli@gmail.com>
2025-12-23 18:40:21 +0530

    it can work only with city name also so remove other inputs state code and country code

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,82 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
- const [cityName,setCityName] = useState("");
- const [stateCode,setStateCode] = useState("");
- const [countryCode,setCountryCode] = useState("");
- const [GeoCodeData , setGeoCodeData] = useState([]);
- const [AirPollutionData, setAirPollutionData] = useState([]);
- const [search,setSearch] = useState(false);
- //fetch geocodedata
-//  useEffect(()=>{
-//    if(!search) return ;
-//    if(!cityName || !stateCode || !countryCode) return;
-//   const fetchData = async ()=>{
-//     try {
-//      const response = await fetch(`${process.env.GEOCODING_API}?q=${cityName.trim()},${stateCode.trim()},${countryCode.trim()}&limit=1&appid=${process.env.API_KEY}`);
-//      if(!response.ok) throw new Error(`HTTP error! status: ${response.status}`)
-//      const data = await response.json();
-//      setGeoCodeData(data);
-//     } catch (error) {
-//       console.log(`Unable to fetch data ${error.message}`) ;
-//     }
-//   }
-//    fetchData();
-//  },[search])
+  //importing api key and endpoints
+  const GEOCODING_API = import.meta.env.VITE_GEOCODING_API;
+  const AIR_POLLUTION_API = import.meta.env.VITE_AIR_POLLUTION_API;
+  const API_KEY = import.meta.env.VITE_API_KEY;
+  const [cityName, setCityName] = useState("");
+  const [stateCode, setStateCode] = useState("");
+  const [countryCode, setCountryCode] = useState("");
+  const [GeoCodeData, setGeoCodeData] = useState([]);
+  const [AirPollutionData, setAirPollutionData] = useState([]);
 
-const searchData = ()=>{
-  if(!cityName || !stateCode || !countryCode) return;
-  const fetchData = async ()=>{
-    try {
-     const response = await fetch(`${process.env.GEOCODING_API}?q=${cityName.trim()},${stateCode.trim()},${countryCode.trim()}&limit=1&appid=${process.env.API_KEY}`);
-     if(!response.ok) throw new Error(`HTTP error! status: ${response.status}`)
-     const data = await response.json();
-     setGeoCodeData(data);
-    } catch (error) {
-      console.log(`Unable to fetch data ${error.message}`) ;
-    }
-  }
-   fetchData();
-}
+  const searchData = () => {
+    const city = cityName.trim();
+    const state = stateCode.trim();
+    const country = countryCode.trim();
+    if (!city || !state || !country) return;
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `${GEOCODING_API}?q=${city},${state},${country}&limit=1&appid=${API_KEY}`
+        );
+        if (!response.ok)
+          throw new Error(`HTTP error! status: ${response.status}`);
+        const data = await response.json();
+        setGeoCodeData(data);
 
- //accessing lat and lon form fetched GeoCodeData
- const lat = GeoCodeData[0]?.lat;
- const lon = GeoCodeData[0]?.lon;
+        //checking if data is present
+        if (!data[0]) {
+          console.log("Data doesnt exist");
+          return;
+        }
+        //accessing lat and lon form fetched GeoCodeData
+        const lat = data[0]?.lat;
+        const lon = data[0]?.lon;
 
-
- //fetching air pollution data
- useEffect(()=>{
-  if(!lat || !lon) return;
-  const fetchData = async ()=>{
-    try {
-     const response = await fetch(`${process.env.AIR_POLLUTION_API}?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}`);
-    if(!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-    const data = await response.json();
-    setAirPollutionData(data) 
-    } catch (error) {
-     console.log(`Unable to fetch data ${error.message}`) ; 
-    }
-  }
-  fetchData();
- },[lat,lon])
-
-
-
+        if (lat && lon) {
+          const airResponse = await fetch(
+            `${AIR_POLLUTION_API}?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+          );
+          if (!airResponse.ok)
+            throw new Error(`HTTP error! status: ${airResponse.status}`);
+          const data = await airResponse.json();
+          setAirPollutionData(data);
+        }
+      } catch (error) {
+        console.log(`Unable to fetch data ${error.message}`);
+      }
+    };
+    fetchData();
+  };
+console.log(GeoCodeData)
+console.log("------------------------");
+console.log(AirPollutionData)
   return (
     <>
-    <input type="text" value={cityName} placeholder='City Name' onChange={(e)=>setCityName(e.target.value)}/>
-    <input type="text" value={stateCode} placeholder='State Code' onChange={(e)=>setStateCode(e.target.value)} />
-    <input type="text" value={countryCode} placeholder='Country Code' onChange={(e)=>setCountryCode(e.target.value)} />
-    <button onClick={()=>searchData()}>Search</button>
+      <input
+        type="text"
+        value={cityName}
+        placeholder="City Name"
+        onChange={(e) => setCityName(e.target.value)}
+      />
+      <input
+        type="text"
+        value={stateCode}
+        placeholder="State Code"
+        onChange={(e) => setStateCode(e.target.value)}
+      />
+      <input
+        type="text"
+        value={countryCode}
+        placeholder="Country Code"
+        onChange={(e) => setCountryCode(e.target.value)}
+      />
+      <button onClick={() => searchData()}>Search</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
